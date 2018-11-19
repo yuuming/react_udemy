bfa18cad2dcb22e1b6b8510ef417f07b6ddebcac
Yuumi <yuuming.n@gmail.com>
2018-11-18 17:19:00 -0800

    Test heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express'); // import express library
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
+    res.send({ hi: 'hehehehahaha'});
 });
 
 const PORT = process.env.PORT || 5000;
