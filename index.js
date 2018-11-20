92c0d7146d49615b412d602346227c77e8ed15a8
Yuumi <yuuming.n@gmail.com>
2018-11-19 23:35:09 -0800

    Create passport function for auth

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express'); // import express library
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientID,
+    callbackURL: '/auth/google/callback'
+}, accessToken => {
+    console.log(accessToken);
+}));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
