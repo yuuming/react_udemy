ed58e3a275e4591839eca968cf12d5fda66c7d12
Yuumi <yuuming.n@gmail.com>
2018-11-19 21:25:09 -0800

    Create keys.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express'); // import express library
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
 const app = express();
 
+passport.use(new GoogleStrategy());
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
