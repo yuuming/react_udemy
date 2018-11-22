a60a4d9ded133712677d715f4952c40737dc02b1
Yuumi <yuuming.n@gmail.com>
2018-11-22 01:46:37 -0800

    Print access, refresh token and profile

--- index.js
+++ index.js
@@ -4,26 +4,28 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
 const app = express();
 passport.use(
- new GoogleStrategy(
- {
- clientID: keys.googleClientID,
- clientSecret: keys.googleClientSecret,
- callbackURL: '/auth/google/callback'
- },
- accessToken => {
- console.log(accessToken);
- }
- )
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
+    }
+  )
 );
 app.get(
- '/auth/google',
- passport.authenticate('google', {
- scope: ['profile', 'email']
- })
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
 );
 
 app.get('/auth/google/callback', passport.authenticate('google'));
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
- console.log(`running on ${port}`);
+  console.log(`running on ${port}`);
 });
