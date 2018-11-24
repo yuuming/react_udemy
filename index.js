6904e73dc3189381ee52f38fd38ee4d71508228e
Yuumi <yuuming.n@gmail.com>
2018-11-23 23:57:18 -0800

    Organize files and functions

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+require('./services/passport');
 
 const app = express();
-
+require('.routes/authRoutes')(app);
+authRoutes(app);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,11 @@
-app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-      scope: ['profile', 'email']
-    })
-  );
-  
-  app.get('/auth/google/callback', passport.authenticate('google'));
+const passport = require('passport');
+module.exports = app => {
+    app.get(
+        '/auth/google',
+        passport.authenticate('google', {
+          scope: ['profile', 'email']
+        })
+      );
+      app.get('/auth/google/callback', passport.authenticate('google'));   
+}
+
