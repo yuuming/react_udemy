d00a538488524c0aae0792a1969988a133bcc563
Yuumi <yuuming.n@gmail.com>
2018-11-24 23:20:40 -0800

    Add comma

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express');
-require('./services/passport');
-
+const passport = require('passport');
 const app = express();
 require('.routes/authRoutes')(app);
-authRoutes(app);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`running on ${port}`);
-});
+app.listen(port);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,11 @@
-const passport = require('passport');
-module.exports = app => {
+import { authenticate } from 'passport';
+export default app => {
     app.get(
         '/auth/google',
-        passport.authenticate('google', {
+        authenticate('google', {
           scope: ['profile', 'email']
         })
       );
-      app.get('/auth/google/callback', passport.authenticate('google'));   
-}
+      app.get('/auth/google/callback', authenticate('google'));   
+};
 
