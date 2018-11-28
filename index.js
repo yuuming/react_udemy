1e1e56c67e227efa43387f319a66eda0e4d83825
Yuumi <yuuming.n@gmail.com>
2018-11-27 16:57:23 -0800

    Save users collections

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
-require('./services/passport');
 require('./models/User');
+require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
-require('./routes/authRoutes')(app);
+require('./routes/authRoutes')(app); 
 
 const port = process.env.PORT || 5000;
 app.listen(port);
