5ade0cbbb604ef6c6ebb3d73d61cfeb4c36d8e03
Yuumi <yuuming.n@gmail.com>
2018-12-03 00:33:20 -0800

    Implement auth flow

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID: '1056279458856-bojgpb5eq1ctkdrphoe7eo4s8kcslc1c.apps.googleusercontent.com',
+    googleClientSecret: '167wiyPcZ3uDi-9w0h6GCuzx',
+    mongoURI: 'mongodb://yuumi:password1@ds115263.mlab.com:15263/emaily_dev',
+    cookieKey: 'ehkqrnlskmnkfkskalafgrfnwkjnn'
+};
+
---
+++ config/prod.js
@@ -0,0 +1,9 @@
+// prod.js - profuction keys here!
+// Environment variable usually capital with under score 
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
